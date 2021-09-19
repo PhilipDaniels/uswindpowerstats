2b9c8008596988966813b2ae456daf9c3313b111
Philip Daniels <Philip.Daniels1971@gmail.com>
2021-09-19 11:28:52 +0100

    Remove unnecessary model conversion functions.

--- rustworkspace/repository/src/models.rs
+++ rustworkspace/repository/src/models.rs
@@ -9,16 +9,6 @@ pub struct ImageSource {
     pub name: String,
 }
 
-impl TryFrom<Row> for ImageSource {
-    type Error = crate::error::Error;
-    
-    fn try_from(row: Row) -> Result<Self, Self::Error> {
-        let id = row.try_get::<u8, _>(0)?.unwrap();
-        let name = row.try_get::<&str, _>(1)?.unwrap().to_string();
-        Ok(ImageSource { id, name })
-    }
-}
-
 impl TryFrom<&Row> for ImageSource {
     type Error = crate::error::Error;
 
@@ -26,5 +16,24 @@ impl TryFrom<&Row> for ImageSource {
         let id = row.try_get::<u8, _>(0)?.unwrap();
         let name = row.try_get::<&str, _>(1)?.unwrap().to_string();
         Ok(ImageSource { id, name })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Serialize, Deserialize)]
+pub struct County {
+    pub id: i32,
+    pub state_id: String,
+    pub name: String,
+}
+
+impl TryFrom<&Row> for County {
+    type Error = crate::error::Error;
+
+    fn try_from(row: &Row) -> Result<Self, Self::Error> {
+        let id = row.try_get::<i32, _>(0)?.unwrap();
+        let state_id = row.try_get::<&str, _>(1)?.unwrap().to_string();
+        let name = row.try_get::<&str, _>(2)?.unwrap().to_string();
+        Ok(County { id, state_id, name })
     }
 }
